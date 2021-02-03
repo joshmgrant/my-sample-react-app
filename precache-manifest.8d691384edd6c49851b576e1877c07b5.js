cd9406078f1da0d93ae6b09a670985e50a4bf6b6
joshgrant <joshua.m.grant@gmail.com>
2021-02-03 15:41:13 -0500

    Updates

---
+++ precache-manifest.8d691384edd6c49851b576e1877c07b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a010c1a5f90118c80ef174542eaf02c",
+    "url": "/my-sample-react-app/index.html"
+  },
+  {
+    "revision": "2056324dd03b9085d6f6",
+    "url": "/my-sample-react-app/static/css/main.4bf56663.chunk.css"
+  },
+  {
+    "revision": "76ff6d704c1e469706be",
+    "url": "/my-sample-react-app/static/js/2.655217e4.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/my-sample-react-app/static/js/2.655217e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2056324dd03b9085d6f6",
+    "url": "/my-sample-react-app/static/js/main.9e72ee38.chunk.js"
+  },
+  {
+    "revision": "544ef69cd888051456e8",
+    "url": "/my-sample-react-app/static/js/runtime-main.99bfb3d2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-sample-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
